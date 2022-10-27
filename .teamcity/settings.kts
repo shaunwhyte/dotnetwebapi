899c718279117d7b5e2c3bf693960d14b2de7734
shaun <shaun@TeamCity>
2022-10-27 21:53:06 +0000

    TeamCity change in 'Dotnetwebapi' project: 1 build step(s) were added to 'Build From Kotlin Yay' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -35,6 +36,13 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetBuild {
+            projects = "WebAPI/WebAPI.sln"
+            sdk = "6"
+        }
     }
 
     triggers {
